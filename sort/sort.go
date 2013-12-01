c1b71250d08ee75b8e4eb097117bde8e82c485ac
Peter Yanovich <fl00r@yandex.ru>
2013-12-02 01:38:08 +0400

    ShellSort

--- sort/sort.go
+++ sort/sort.go
@@ -1,32 +1,52 @@
 package sort
+// import( "fmt" )
 
 func SelectionSort(data []int) {
-	var smallest, smallest_val int
 	n := len(data)
 	for i := 0; i < n; i++ {
-		smallest = i
 		for j := i+1; j < n; j++ {
-			if(data[j] < data[smallest]) {
-				smallest = j
+			if(less(data, j, i)) {
+				swap(data, i, j)
 			}
 		}
-		smallest_val = data[smallest]
-		data[smallest] = data[i]
-		data[i] = smallest_val
 	}
 }
 
 func InsertionSort(data []int) {
-	var smallest, smallest_val int
 	n := len(data)
-	for i := 0; i < n; i++ {
-		val := data[i]
-		smallest = i
-		for j := i-1; j > 0 && val < data[j]; j-- {
-			smallest = j
+	for i := 1; i < n; i++ {
+		for j := i-1; j >= 0 && less(data, j+1, j); j-- {
+			swap(data, j+1, j)
 		}
-		smallest_val = data[smallest]
-		data[smallest] = data[i]
-		data[i] = smallest_val
 	}
+}
+
+func ShellSort(data []int) {
+	n := len(data)
+	h := 1
+	for h < n/3 {
+		h = 3*h + 1
+	}
+	for h >= 1 {
+		for i := 1; i < n; i++ {
+			for j := i-1; j >= 0 && less(data, j+1, j); j -= h {
+				swap(data, j+1, j)
+			}
+		}
+		h = h / 3
+	}
+}
+
+/*
+	private party
+*/
+
+func swap(data []int, i, j int) {
+	i_val := data[i]
+	data[i] = data[j]
+	data[j] = i_val
+}
+
+func less(data []int, i, j int) bool {
+	return data[i] < data[j]
 }
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -5,19 +5,29 @@ import (
 )
 
 func TestLinkedListStack(t *testing.T) {
-	data := []int{ 1, 3, 2, 5, 4 }
+	var data []int
+
+	data = []int{ 5, 3, 2, 1, 4 }
 	SelectionSort(data)
 	for i, v := range(data) {
 		if i+1 != v {
-			t.Errorf("%d should be equeal to %d", v, i+1)
+			t.Errorf("SelectionSort: %d should be equal to %d for %d", v, i+1, data)
 		}
 	}
 	
-	data = []int{ 1, 3, 2, 5, 4 }
+	data = []int{ 5, 3, 2, 1, 4 }
 	InsertionSort(data)
 	for i, v := range(data) {
 		if i+1 != v {
-			t.Errorf("%d should be equeal to %d", v, i+1)
+			t.Errorf("InsertionSort: %d should be equal to %d for %d", v, i+1, data)
+		}
+	}
+	
+	data = []int{ 5, 3, 2, 1, 4 }
+	ShellSort(data)
+	for i, v := range(data) {
+		if i+1 != v {
+			t.Errorf("ShellSort: %d should be equal to %d for %d", v, i+1, data)
 		}
 	}
 }
